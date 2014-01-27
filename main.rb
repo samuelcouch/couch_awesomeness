59abbfbc5d97903271d40f57544aa5756d86f7f2
Sam Couch <samcouch91@gmail.com>
2014-01-27 14:27:18 -0500

    Added random numbers for awesomeness

--- main.rb
+++ main.rb
@@ -1,20 +1,20 @@
 require './models/person'
 require './models/awesomeness'
 
-people = [Person.new("joe", 1), 
-              Person.new("bob", 2),
-              Person.new("sam", 3),
-              Person.new("tom", 4),
-              Person.new("fred", 5),
-              Person.new("ross", 6),
-              Person.new("jack", 7),
-              Person.new("frank", 8),
-              Person.new("will", 9),
-              Person.new("tim", 10),
-              Person.new("dog", 11),
-              Person.new("rat", 12),
-              Person.new("job", 13), 
-              Person.new("bo", 14)]
+people = [Person.new("joe", Random.rand(100)), 
+              Person.new("bob", Random.rand(100)),
+              Person.new("sam", Random.rand(100)),
+              Person.new("tom", Random.rand(100)),
+              Person.new("fred", Random.rand(100)),
+              Person.new("ross", Random.rand(100)),
+              Person.new("jack", Random.rand(100)),
+              Person.new("frank", Random.rand(100)),
+              Person.new("will", Random.rand(100)),
+              Person.new("tim", Random.rand(100)),
+              Person.new("dog", Random.rand(100)),
+              Person.new("rat", Random.rand(100)),
+              Person.new("job", Random.rand(100)), 
+              Person.new("bo", Random.rand(100))]
 
 awesomeness_demo = Awesomeness.new(people)
 
